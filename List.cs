fbe3709fc725d496cdeee6e366879ea51ef9a517
Sasha Goncharik <sasha220204@mail.ru>
2022-11-11 14:27:27 +0300

    added class List with List FUCK yep

---
+++ List.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beauty_Salon
+{
+    public class List
+    {
+        public List<int> FUCK = new List<int>();
+    }
+}
